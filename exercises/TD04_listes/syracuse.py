ab601d4718069d9ada626f5a8fae1e6adb990bc5
Alann MONNIER <alann.monnier-blondeau@ens.uvsq.fr>
2021-10-26 13:35:10 +0200

    TD

--- exercises/TD04_listes/syracuse.py
+++ exercises/TD04_listes/syracuse.py
@@ -1,7 +1,6 @@
 def syracuse(n):
     """ Retourne la liste des valeurs de la suite en partant de n jusqu'à 1 """
     liste = [n]
-    print(liste)
     while n != 1:
         if n % 2 == 0:
             n //= 2
@@ -9,13 +8,15 @@ def syracuse(n):
         else:
             n = n * 3 + 1 
             liste.append(n)
-    print(liste)
+    return liste
         
-
 print(syracuse(3))
 
 def testeConjecture(n_max):
     """ Teste la conjecture de Collatz pour toutes les valeurs de 2 à n_max """
-    pass
+    for i in range(2, n_max + 1):
+        syracuse(i)
+        if liste[-1] == 1:
+            return True
 
-#print(testeConjecture(10000))
+print(testeConjecture(10000))
