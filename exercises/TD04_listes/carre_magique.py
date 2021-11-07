f6da9391e3eb154502a8fd4214bc43b2ae21535a
Alann MONNIER <alann.monnier-blondeau@ens.uvsq.fr>
2021-11-07 19:08:46 +0100

    TD

--- exercises/TD04_listes/carre_magique.py
+++ exercises/TD04_listes/carre_magique.py
@@ -4,8 +4,8 @@ def afficheCarre(carre):
         print('   '.join([str(j) for j in i]))
 
 
-carre_mag = ((4, 14, 15, 1), (9, 7, 6, 12), (5, 11, 10, 8), (16, 2, 3, 13))
-carre_pas_mag = ((4, 14, 15, 1), (9, 7, 6, 12), (5, 11, 10, 8), (16, 2, 7, 13))
+carre_mag = ([4, 14, 15, 1], [9, 7, 6, 12], [5, 11, 10, 8], [16, 2, 3, 13])
+carre_pas_mag = ([4, 14, 15, 1], [9, 7, 6, 12], [5, 11, 10, 8], [16, 2, 7, 13])
 #afficheCarre(carre_mag)
 #print("")
 #afficheCarre(carre_pas_mag)
@@ -16,17 +16,21 @@ def testLignesEgales(carre):
      ont la même somme, et -1 sinon """
     somme = 0
     liste = []
+    j = 0
     for i in range(0, len(carre)):
-        for j in range(0, len(carre)):
-            somme += int(carre[j][i])
+        while j != 4:
+            somme += int(carre[i][j])
+            j += 1
         liste.append(somme)
-        print(somme)
+        j = 0 
+        somme = 0
+
     for k in range(0, len(liste)):
-        if liste[k] == liste[k + 1]:
-            return somme
+        if liste[k] == liste[-1]:
+            return liste[k]
         else:
-            return - 1
-            
+            return -1
+
 
 print(testLignesEgales(carre_mag))
-#print(testLignesEgales(carre_pas_mag))
+print(testLignesEgales(carre_pas_mag))
