5c2d3f332320ce6e391ff04cea726b833a851fe5
Alann MONNIER <alann.monnier-blondeau@ens.uvsq.fr>
2021-11-27 17:38:25 +0100

    TD

--- exercises/TD05_gui/dessin.py
+++ exercises/TD05_gui/dessin.py
@@ -1,26 +1,51 @@
 import tkinter as tk
+import random
 
 ecran = tk.Tk()
 ecran.title("Mon dessin")
 
-ecran.geometry("900x700")
+ecran.geometry("600x600")
 ecran.configure(bg="grey")
+
 button_1 = tk.Button(ecran, text="Choisir une couleur", font = ("helvetica", "25"), bg="blue")
-button_1.grid(row=0, column=1,columnspan=3)
+button_1.grid(row=0, column=1, columnspan=2, pady=5)
 
-button_2 = tk.Button(ecran, text="Cercle", font = ("helvetica", "25"), bg="black", fg="white")
-button_2.grid(column=0, row=1, pady=20)
+def Carres():
+    x0 = random.randint(0, (width-100))
+    y0 = random.randint(0, (height-100))
+    carre = tk.Canvas.create_rectangle(fond_noir, (x0,y0), ((100 + x0), (100 + y0)), fill="red")
 
-button_3 = tk.Button(ecran, text="Carré", font = ("helvetica", "25"), bg="black", fg="white")
-button_3.grid(column=0, row=2, pady=20)
+def Croixs():
+    x0 = random.randint(0, (width-100))
+    y0 = random.randint(0, (height-100))
+    carre = tk.Canvas.create_rectangle(fond_noir, (x0,y0), ((100 + x0), (100 + y0)), fill="white")
+    ligne_1 = tk.Canvas.create_line(fond_noir, (x0, y0), ((x0 + 100), (y0 + 100)), fill="yellow")
+    ligne_2 = tk.Canvas.create_line(fond_noir, ((x0+100), y0), (x0, (y0 + 100)), fill="yellow")
 
-button_4 = tk.Button(ecran, text="Croix", font = ("helvetica", "25"), bg="black", fg="white")
-button_4.grid(column=0, row=3, pady=20)
+def Cercles():
+    x0 = random.randint(0, (width-100))
+    y0 = random.randint(0, (height-100))
+    cercle = tk.Canvas.create_oval(fond_noir, (x0, y0), ((100 + x0), (100 + y0)), fill="blue")
+    
 
-fond_noir = tk.Canvas(ecran, bg = 'black')
-fond_noir.grid(row=1, rowspan=4, column=1, columnspan=2, padx=5)   
 
+button_2 = tk.Button(ecran, text="Cercle", font=("helvetica", "25"), bg="red", fg="white", command=Cercles)
+button_2.grid(column=0, row=1, pady=10)
 
+button_3 = tk.Button(ecran, text="Carré", font=("helvetica", "25"), bg="red", fg="white", command=Carres)
+button_3.grid(column=0, row=2, pady=10)
 
+button_4 = tk.Button(ecran, text="Croix", font=("helvetica", "25"), bg="red", fg="white", command=Croixs)
+button_4.grid(column=0, row=3, pady=10)
+
+fond_noir = tk.Canvas(ecran, bg="black", borderwidth=3, relief="groove", highlightbackground="white")
+fond_noir.grid(row=1, rowspan=3, column=1, columnspan=2, padx=5, pady=5)   
+
+# fonction qui permet de connaître les dimensions d'un widget
+fond_noir.update()
+# Récupére la largeur
+width = fond_noir.winfo_width() 
+# Récupére la hauteur
+height = fond_noir.winfo_height()
 
 ecran.mainloop()
