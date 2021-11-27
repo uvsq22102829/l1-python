d71ee08b3cd2c36bc74c090dbcac2bae628b47f2
Alann MONNIER <alann.monnier-blondeau@ens.uvsq.fr>
2021-11-27 21:27:45 +0100

    TD

--- exercises/TD05_gui/cible.py
+++ exercises/TD05_gui/cible.py
@@ -11,17 +11,21 @@ ecran.title("Cible")
 
 
 color = ["blue", "green", "black", "yellow", "magenta", "red"]
-canvas = tk.Canvas(ecran, height=800, width= 800, bg="black")
+canvas = tk.Canvas(ecran, height=800, width= 800, bg="grey")
 canvas.grid()
 longueur = 0
+a = 0
 j = 0
-for i in range(0, len(nombre_cercle) + 1):
-    cercle = tk.Canvas.create_oval(canvas, ((0 + longueur), (0 + longueur)), ((int(width) - longueur), (int(width) - longueur)), fill=color[j])
-    if j == len(color):
-        j = 0
-    j += 1
-    longueur += int(width) // int(nombre_cercle) 
-    print(longueur)
-    print(i)
+
+while a != int(nombre_cercle):
+        cercle = tk.Canvas.create_oval(canvas, ((0 + longueur), (0 + longueur)), ((int(width) - longueur), (int(width) - longueur)), fill=color[j])
+        print(j)
+        if j == len(color) - 1:
+            j = 0
+        j += 1
+        longueur += (int(width) // 2) // int(nombre_cercle) 
+        a += 1
+        
+
 
 ecran.mainloop() 
