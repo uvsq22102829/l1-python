9a6d8067e00be6777ea41d4ef9ee93d6ca1c6c99
Alann MONNIER <alann.monnier-blondeau@ens.uvsq.fr>
2021-11-27 20:01:21 +0100

    TD

--- exercises/TD05_gui/cible.py
+++ exercises/TD05_gui/cible.py
@@ -1,16 +1,27 @@
 import tkinter as tk
 
 
+width = input("Entrer la taille du cercle")
+nombre_cercle = input("Entrer le nombre de cercle que vous voulez")
+
 ecran = tk.Tk()
 
 ecran.geometry("800x800")
+ecran.title("Cible")
 
 
 color = ["blue", "green", "black", "yellow", "magenta", "red"]
-
-canvas = tk.Canvas(ecran, height="800", width="800") 
+canvas = tk.Canvas(ecran, height=800, width= 800, bg="black")
 canvas.grid()
-cercle = tk.Canvas.create_oval(canvas, (0,0), (800,800), overline=color[0])
+longueur = 0
+j = 0
+for i in range(0, len(nombre_cercle) + 1):
+    cercle = tk.Canvas.create_oval(canvas, ((0 + longueur), (0 + longueur)), ((int(width) - longueur), (int(width) - longueur)), fill=color[j])
+    if j == len(color):
+        j = 0
+    j += 1
+    longueur += int(width) // int(nombre_cercle) 
+    print(longueur)
+    print(i)
 
-ecran.mainloop()
-
+ecran.mainloop() 
