333505fcde49deffd8ae7a2fdbebfc9664ce5288
Alann MONNIER <alann.monnier-blondeau@ens.uvsq.fr>
2021-11-28 19:07:10 +0100

    TD

--- exercises/TD05_gui/couleur.py
+++ exercises/TD05_gui/couleur.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 
 ecran = tk.Tk()
 ecran.geometry("600x500")
@@ -9,13 +10,31 @@ def get_color(r, g, b):
 
 
 def draw_pixel(i, j, color):
-    tk.Canvas(canvas, (i, j), bg=color)
+    canvas_2 = tk.Canvas.create_line(canvas, i, j, i, j, fill=color)
+    
+
+def ecran_aleatoire():
+    a = 0
+    i = 0
+    j = 0
+    while a != (256 * 256):
+        while i != 256:
+            r = random.randint(0, 255)
+            g = random.randint(0, 255)
+            b = random.randint(0, 255)
+            color = get_color(r, g, b)
+            draw_pixel(i, j, color)
+            i += 1
+            print(a)
+            a += 1
+        i = 0
+        j +=1
 
     
 
 
 
-button_1 = tk.Button(ecran, text="Aléatoire", fg="blue", bg="white", font=(20))
+button_1 = tk.Button(ecran, text="Aléatoire", fg="blue", bg="white", font=(20), command=ecran_aleatoire)
 button_1.grid(row = 0, column=0, pady=20)
 
 button_2 = tk.Button(ecran, text="Dégradé gris", fg="blue", bg="white", font=20)
@@ -27,6 +46,6 @@ button_3.grid(row = 2, column= 0, pady=20)
 canvas = tk.Canvas(ecran, bg="black", height= 256, width=256)
 canvas.grid(column = 1, row= 0, rowspan=3)
 
-draw_pixel((344 + (256//2)), (244 + (256//2)), get_color(255,255,255))
+draw_pixel(128, 128, (get_color(255,255,255)))
 
 ecran.mainloop()
