c285db1323333a25d48a7418775113b8eec29558
Alann MONNIER <alann.monnier-blondeau@ens.uvsq.fr>
2021-11-29 21:55:17 +0100

    TD

---
+++ exercises/TD05_gui/clic.py/clic_1.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+ecran = tk.Tk()
+ecran.geometry("700x700")
+
+
+def get_color(r, g, b):
+    """ Retourne une couleur à partir de ses composantes r, g, b entre 0 et 255"""
+    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
+
+def pixel(event):
+    r = 255
+    g = 0
+    b = 0
+    color = get_color(r, g, b)
+    event.x = color
+    event.y = color 
+
+canvas = tk.Canvas(ecran, height=500, width=500, bg="black")
+canvas.grid()
+canvas.bind("<Button-1>", pixel)
+
+ecran.mainloop()
